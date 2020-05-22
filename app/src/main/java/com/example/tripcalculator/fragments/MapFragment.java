3d200121f5d68ac53022d7d4fc9602d8b7481300
Maxim Derevyanchenko <maxim.derevyanchenko@studio.unibo.it>
2020-05-22 18:05:17 +0200

    Add rotation button and checking if GPS is activated

--- app/src/main/java/com/example/tripcalculator/fragments/MapFragment.java
+++ app/src/main/java/com/example/tripcalculator/fragments/MapFragment.java
@@ -22,6 +22,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.location.LocationManagerCompat;
 import androidx.preference.PreferenceManager;
@@ -50,6 +51,7 @@ import org.osmdroid.views.overlay.Polyline;
 import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
 import org.osmdroid.views.overlay.infowindow.MarkerInfoWindow;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.IMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.util.ArrayList;
@@ -71,6 +73,8 @@ public class MapFragment extends MapViewFragment {
     private int nextLocationIndex = 0;
     private boolean hasPermissions = false;
     private IMapController mapController;
+    //GPS
+    private boolean isGPSOn = false;
 
     @SuppressLint("MissingPermission")
     @Nullable
@@ -86,59 +90,29 @@ public class MapFragment extends MapViewFragment {
 
         map.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
         map.setMultiTouchControls(true);
-        RotationGestureOverlay mRotationGestureOverlay = new RotationGestureOverlay(map);
-        mRotationGestureOverlay.setEnabled(true);
-        map.getOverlays().add(mRotationGestureOverlay);
 
         mapController = map.getController();
         mapController.setZoom(9.5);
         binding.checkPositionBtn.setVisibility(View.VISIBLE);
         binding.myPositionBtn.setVisibility(View.GONE);
-        GeoPoint startPoint;
+        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
         checkPermissions();
+        checkGPS();
         GpsMyLocationProvider provider = new GpsMyLocationProvider(requireContext());
         mLocationOverlay = new MyLocationNewOverlay(provider, map);
-
-        if(hasPermissions){
-
+        if(isGPSOn){
+            map.getOverlays().add(mLocationOverlay);
             mLocationOverlay.enableMyLocation();
-            map.getOverlayManager().add(mLocationOverlay);
-            startPoint = mLocationOverlay.getMyLocation();
+            if (mLocationOverlay.getMyLocation() != null)
+                startPoint = mLocationOverlay.getMyLocation();
         } else {
-            startPoint = new GeoPoint(48.8583, 2.2944);
+            showActivateGPSDialog();
         }
         mapController.setCenter(startPoint);
         map.setTileSource(TileSourceFactory.MAPNIK);
 
-        Overlay overlay = new Overlay() {
-            @Override
-            public boolean onSingleTapConfirmed(MotionEvent e, MapView mapView) {
-                if (path == null) {
-                    Projection proj = mapView.getProjection();
-                    GeoPoint loc = (GeoPoint) proj.fromPixels((int) e.getX(), (int) e.getY());
-                    double longitude = loc.getLongitude();
-                    double latitude = loc.getLatitude();
-                    createRequest(latitude + " " + longitude);
-                }
-                return true;
-            }
-        };
-
         initMapLayout();
 
-        map.getOverlays().add(overlay);
-        binding.checkPositionBtn.setOnClickListener(v -> {
-            if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)){
-                checkPermissions();
-            } else {
-                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts("package", requireActivity().getPackageName(), null));
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-        });
-        binding.myPositionBtn.setOnClickListener(v -> {
-            mapController.setCenter(mLocationOverlay.getMyLocation());
-        });
         return binding.getRoot();
     }
 
@@ -146,14 +120,17 @@ public class MapFragment extends MapViewFragment {
     public void onResume() {
         super.onResume();
         map.onResume();
-        mLocationOverlay.enableMyLocation();
+        checkGPS();
+        if (isGPSOn)
+            mLocationOverlay.enableMyLocation();
     }
 
     @Override
     public void onPause() {
         super.onPause();
         map.onPause();
-        mLocationOverlay.disableMyLocation();
+        if (isGPSOn)
+            mLocationOverlay.disableMyLocation();
     }
 
     public void clearMarkers() {
@@ -198,17 +175,75 @@ public class MapFragment extends MapViewFragment {
             map.getOverlays().add(marker);
             i++;
         }
-
-        if (hasPermissions){
-            this.mLocationOverlay.enableMyLocation();
-            map.getOverlays().add(this.mLocationOverlay);
-        }
     }
 
     private void initMapLayout(){
+
+        Overlay overlay = new Overlay() {
+            @Override
+            public boolean onSingleTapConfirmed(MotionEvent e, MapView mapView) {
+                if (path == null) {
+                    Projection proj = mapView.getProjection();
+                    GeoPoint loc = (GeoPoint) proj.fromPixels((int) e.getX(), (int) e.getY());
+                    double longitude = loc.getLongitude();
+                    double latitude = loc.getLatitude();
+                    createRequest(latitude + " " + longitude);
+                }
+                return true;
+            }
+        };
+        map.getOverlays().add(overlay);
+
+        RotationGestureOverlay mRotationGestureOverlay = new RotationGestureOverlay(map);
+        mRotationGestureOverlay.setEnabled(true);
+        map.getOverlays().add(mRotationGestureOverlay);
+
+        binding.disableRotationBtn.setOnClickListener(v -> {
+            mRotationGestureOverlay.setEnabled(false);
+            binding.toNorthBtn.performClick();
+            binding.disableRotationBtn.setVisibility(View.GONE);
+            binding.enableRotationBtn.setVisibility(View.VISIBLE);
+        });
+        binding.enableRotationBtn.setOnClickListener(v -> {
+            mRotationGestureOverlay.setEnabled(true);
+            binding.enableRotationBtn.setVisibility(View.GONE);
+            binding.disableRotationBtn.setVisibility(View.VISIBLE);
+        });
+
         binding.zoomOutBtn.setOnClickListener(v -> showAllMarkers());
         binding.toNorthBtn.setOnClickListener(v -> map.getController().animateTo(map.getMapCenter(), map.getZoomLevelDouble(), 2000L, 0.0F));
         binding.clearMarkersBtn.setOnClickListener(v -> clearMarkers());
+
+        binding.checkPositionBtn.setOnClickListener(v -> {
+            if (!hasPermissions){
+                if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)){
+                    checkPermissions();
+                } else {
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts("package", requireActivity().getPackageName(), null));
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            } else if (!isGPSOn){
+                checkGPS();
+                if (!isGPSOn)
+                    showActivateGPSDialog();
+                else
+                    binding.myPositionBtn.performClick();
+            }
+        });
+        binding.myPositionBtn.setOnClickListener(v -> {
+            checkGPS();
+            if (isGPSOn) {
+                if (mLocationOverlay.getMyLocation() != null) {
+                    focusOn(mLocationOverlay.getMyLocation());
+                    if (path == null)
+                        createRequest(mLocationOverlay.getMyLocation().getLatitude() + " " + mLocationOverlay.getMyLocation().getLongitude());
+                } else {
+                    Snackbar.make(requireActivity().findViewById(R.id.map_fragment), "Impossibile ottenere la posizione", Snackbar.LENGTH_LONG).show();
+                }
+            } else
+                showActivateGPSDialog();
+        });
     }
 
     private void showAllMarkers() {
@@ -216,15 +251,20 @@ public class MapFragment extends MapViewFragment {
         for (Marker marker : markers){
             points.add(marker.getPosition());
         }
-        if(points.size() > 0) {
+        if(points.size() > 1) {
             Road road = new Road(points);
             map.zoomToBoundingBox(road.mBoundingBox, true);
+        } else if (points.size() == 1){
+            focusOn(points.get(0));
         }
     }
 
     public void focusOn(Location location) {
-        IMapController mapController = map.getController();
         GeoPoint point = new GeoPoint(location.Latitude, location.Longitude);
+        focusOn(point);
+    }
+
+    private void focusOn(GeoPoint point){
         mapController.animateTo(point, 15.0, 2000L, map.getMapOrientation());
     }
 
@@ -257,31 +297,42 @@ public class MapFragment extends MapViewFragment {
                 nextLocationIndex = i;
                 focusOn(path.get(nextLocationIndex));
             }
-            if (hasPermissions)
+            if (hasPermissions) {
                 startNavigation();
+            }
         } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void checkGPS(){
+        if (hasPermissions){
+            LocationManager manager = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
+            isGPSOn = manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            if (isGPSOn){
+                binding.checkPositionBtn.setVisibility(View.GONE);
+                binding.myPositionBtn.setVisibility(View.VISIBLE);
+            } else {
+                binding.myPositionBtn.setVisibility(View.GONE);
+                binding.checkPositionBtn.setVisibility(View.VISIBLE);
+            }
         }
     }
 
     private void checkPermissions(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                    ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-                    //ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION_PERMISSIONS);
+                    ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION}, REQUEST_LOCATION_PERMISSIONS);
             } else {
-                binding.checkPositionBtn.setVisibility(View.GONE);
-                binding.myPositionBtn.setVisibility(View.VISIBLE);
                 hasPermissions = true;
             }
         } else {
             if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                     ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION_PERMISSIONS);
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION_PERMISSIONS);
             } else {
-                binding.checkPositionBtn.setVisibility(View.GONE);
-                binding.myPositionBtn.setVisibility(View.VISIBLE);
                 hasPermissions = true;
             }
         }
@@ -292,16 +343,20 @@ public class MapFragment extends MapViewFragment {
         if (requestCode == REQUEST_LOCATION_PERMISSIONS){
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 hasPermissions = true;
-                binding.checkPositionBtn.setVisibility(View.GONE);
-                binding.myPositionBtn.setVisibility(View.VISIBLE);
             }else{
-                Snackbar.make(requireActivity().findViewById(R.id.map_fragment), "Permessi negati!\nLa tua posizione non può essere visualizzata sulla mappa!\nL'arrivo dovrà essere indicato manualmente!", Snackbar.LENGTH_LONG).show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+                builder.setTitle("Permessi negati!")
+                        .setMessage("La tua posizione non può essere visualizzata sulla mappa!\n" +
+                                "L'arrivo dovrà essere indicato manualmente!\n" +
+                                "Per fornire i permessi cliccare sul simbolo del GPS sulla sua destra!" )
+                        .setNeutralButton("OK", (dialog, which) -> {}).show();
             }
         }
     }
 
     private void startNavigation(){
         mLocationOverlay.getMyLocationProvider().startLocationProvider((location, source) -> {
+            mLocationOverlay.onLocationChanged(location, source);
             android.location.Location nextLocation = new android.location.Location(PROVIDER);
             nextLocation.setLatitude(path.get(nextLocationIndex).Latitude);
             nextLocation.setLongitude(path.get(nextLocationIndex).Longitude);
@@ -310,6 +365,24 @@ public class MapFragment extends MapViewFragment {
                 mLocationOverlay.getMyLocationProvider().stopLocationProvider();
             }
         });
+    }
+
+    private void showActivateGPSDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("GPS disattivato!")
+                .setMessage("Vuoi attivare il GPS?")
+                .setPositiveButton(R.string.settings, (dialog, which) -> setSettingsIntent())
+                .setNegativeButton(R.string.close, (dialog, which) -> {}).show();
+    }
+
+    private void setSettingsIntent(){
+        Intent intent = new Intent();
+        intent.setAction((Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        if(intent.resolveActivity(requireActivity().getPackageManager()) != null){
+            requireActivity().startActivity(intent);
+        }
     }
 
     @Override
